9abe69b18a983aade52f7666be333b33159e3085
gp-hub321 <148525451+gp-hub321@users.noreply.github.com>
2023-10-20 22:16:01 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 //testing
+//testing2
 @SpringBootApplication
 @RestController
 public class DevopsIntegrationApplication {
